171ce48ca10f83196b19afd58027bb89d166848e
TimekillerTK <erwartungen@protonmail.com>
2020-09-13 11:52:11 +0200

    first working version, help broken

--- Get-TVEpList.ps1
+++ Get-TVEpList.ps1
@@ -1,10 +1,6 @@
-# For categorizing show info
-# Create a cmdlet that will retrieve info from whatever source online and spit it out with an object, with the episode number, season, show name, etc.. This kind of info will be used by other cmdlets down the line.  
-
 # Function
 function Get-TVEpList {
-# Help is broken fix later
-<#
+    <#
     .SYNOPSIS
     Write synopsis of what this does
 
@@ -33,7 +29,34 @@ function Get-TVEpList {
         [Parameter(Mandatory)]
         [string]$URI
     )
+    PROCESS {
+
+        # Web scrape for URI
+        $fetch = Invoke-WebRequest -Uri $URI
+
+        # Selects the relevant links, obviously works only with TV shows, not movies
+        $rawtitles = (($fetch.links).title | Where-Object { $_ -match 'Season ' }) | Select-Object -Unique
+
+        # Create an empty arraylist object
+        $arraylist = New-Object -TypeName "System.Collections.ArrayList"
+        $arraylist.Clear()
+
+        # Template for ConvertFrom-String
+        $template = ("{ShowTitle*:Example Title}: Season {[int]Season:1} ({[int]Date:1988}): Episode {[int]Episode:1} - {EpTitle:Spaghetti Code?}`r`n" +
+                    "{ShowTitle*:Another Example Title}: Season {[int]Season:17} ({[int]Date:2004}): Episode {[int]Episode:42} - {EpTitle:Wonderful, Spices!!!}")
+
+        foreach($item in $rawtitles) {
+            
+            # converts the object 
+            $temp = $item | ConvertFrom-String -TemplateContent $template
+            
+            # add an object to the arraylist
+            $arraylist.Add($temp) | Out-Null
+
+        }
+
+        # Return the result
+        return $arraylist
+
+    }
 }
-
-
-
--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Get-TVEpList
-Retrieve info about episodes for a show
+Retrieve info about episodes for a show, used with https://www.themoviedb.org/ specifically.
 
-Planed features:
-- Create a cmdlet that will retrieve info from whatever source online and spit it out with an object, with the episode number, season, show name, etc.. This kind of info will be used by other cmdlets down the line. 
+## How to use
+Run `Get-TVEpList -URI https://www.themoviedb.org/tv/####/season/##`
+
+## Todo
+- Help is currently broken
+- Uses regex pattern to find relevant info, as a result is fragile and can break easily. Find different solution?
+- Currently only works on TV shows, should work on movies as well
+- Add a parameter `-RemoveIllegalChar` to optionally remove illegal NTFS characters `"/ ? < > \ : * | "`
