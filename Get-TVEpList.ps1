7485734440be380cda30ce434dd87fb36aa84b7b
TimekillerTK <erwartungen@protonmail.com>
2020-09-14 08:40:47 +0200

    Fixed help, added more parameters

--- Get-TVEpList.ps1
+++ Get-TVEpList.ps1
@@ -4,16 +4,34 @@
 function Get-TVEpList {
     <#
     .SYNOPSIS
-    Write synopsis of what this does
+    This tool will fetch individual episode titles from https://www.themoviedb.org
 
     .DESCRIPTION
-    Write description here.
+    Tool for fetching individual episodes from https://www.themoviedb.org
 
     .PARAMETER URI
-    Explain what this paramter does
+    Should be a URI pointing to an individual season of a specific show, like for example: https://www.themoviedb.org/tv/314-star-trek-enterprise/season/1
+
+    .PARAMETER REGEX
+    This regex pattern will be searched for within the URI provided
+
+    .PARAMETER REMOVEILLEGALCHAR
+    Removes NTFS illegal characters / ? < > \ : * | "
 
     .EXAMPLE
-    Example of using the thingy.
+    Get-TVEpList -URI https://www.themoviedb.org/tv/314-star-trek-enterprise/season/1 
+    
+    This example will simply retrieve the episode titles from the URL and return them as an object
+    
+    .EXAMPLE
+    Get-TVEpList -URI https://www.themoviedb.org/tv/314-star-trek-enterprise/season/1 -Regex "Spaghetti"
+    
+    This example uses a custom regex instead of the default "Season "
+
+    .EXAMPLE
+    Get-TVEpList -URI https://www.themoviedb.org/tv/314-star-trek-enterprise/season/1 -RemoveIllegalChars $true
+
+    Will automatically remove NTFS illegal characters from the episode titles: / ? < > \ : * | "
 
     .INPUTS
     String
@@ -26,10 +44,18 @@ function Get-TVEpList {
     Website: https://github.com/TimekillerTK
 #>
 
+    #CmdletBinding turns the function into an advanced function with
     [CmdletBinding()]
     param (
+
         [Parameter(Mandatory)]
-        [string]$URI
+        [string]$URI,
+
+        [Parameter()]
+        [string]$Regex = "Season ",
+
+        [Parameter()]
+        [bool]$RemoveIllegalChar = $false
     )
     PROCESS {
 
@@ -37,13 +63,13 @@ function Get-TVEpList {
         $fetch = Invoke-WebRequest -Uri $URI
 
         # Selects the relevant links, obviously works only with TV shows, not movies
-        $rawtitles = (($fetch.links).title | Where-Object { $_ -match 'Season ' }) | Select-Object -Unique
+        $rawtitles = (($fetch.links).title | Where-Object { $_ -match $Regex }) | Select-Object -Unique
 
         # Create an empty arraylist object
         $arraylist = New-Object -TypeName "System.Collections.ArrayList"
         $arraylist.Clear()
 
-        # Template for ConvertFrom-String
+        # Template for ConvertFrom-String, doesn't work perfectly in all cases, needs to be looked at.
         $template = ("{ShowTitle*:Example Title}: Season {[int]Season:1} ({[int]Date:1988}): Episode {[int]Episode:1} - {EpTitle:Spaghetti Code?}`r`n" +
                     "{ShowTitle*:Another Example Title}: Season {[int]Season:17} ({[int]Date:2004}): Episode {[int]Episode:42} - {EpTitle:Wonderful, Spices!!!}")
 
@@ -51,6 +77,17 @@ function Get-TVEpList {
             
             # converts the object 
             $temp = $item | ConvertFrom-String -TemplateContent $template
+
+            If($RemoveIllegalChar -eq $true) {
+                
+                # NTFS illegal characters for filenames, regex format
+                $illegalchar = "\'", "\""", "\/", "\?", "\<", "\>", "\\", "\:", "\*", "\|" 
+
+                # Loop that goes over each illegal character
+                foreach ($item in $illegalchar) {
+                    $temp.EpTitle = $temp.EpTitle -replace $item,""            
+                }
+            }
             
             # add an object to the arraylist
             $arraylist.Add($temp) | Out-Null
--- README.md
+++ README.md
@@ -5,8 +5,11 @@ Retrieve info about episodes for a show, used with https://www.themoviedb.org/ s
 Run `Get-TVEpList -URI https://www.themoviedb.org/tv/####/season/##`
 
 ## Todo
-- Help is currently broken
+- ~~Help is currently broken~~
+  - Fixed
 - Uses regex pattern to find relevant info, as a result is fragile and can break easily. Find different solution?
-- Instead of using `'Season '` as a match, should prompt the user for what to match for, this will make it useful for scraping other websites as well
+- ~~Instead of using `'Season '` as a match, should prompt the user for what to match for, this will make it useful for scraping other websites as well~~
+  - Instead of prompting, it can be specified with the `-Regex` parameter
 - Currently only works on TV shows, should work on movies as well
-- Add a parameter `-RemoveIllegalChar` to optionally remove illegal NTFS characters `"/ ? < > \ : * | "`
+- ~~Add a parameter `-RemoveIllegalChar` to optionally remove illegal NTFS characters `"/ ? < > \ : * | "`~~
+  - Done
